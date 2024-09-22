019917985c1dd5f3a3e8bd57746fe5c8e57d1d71
Putawan Pao <pardis120@outlook.com>
2024-09-22 22:23:51 +0700

    save

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
 using namespace std;
-void checkprime(); //Define Function
 
-int main() {
-    system ("cls");
-    cout << "This Program Check Prime number " << endl;
-    cout << "--------------------------------" << endl;
-    checkprime(); //Call Function 11
-}
 
-///This main Function
-void checkprime() {
-    int n;
-    cout << "Enter number to check prime number: ";
-    cin >> n;
-        //   2 < 2 2+1
-    for (int i = 2; i < n; i++)
-    {
-        if ( n%i == 0)
-        {
-            cout << n << " Is not Prime Number" << endl;
-        }
-        else
-        {
-            cout << n << " Is Prime number" << endl;
-        }
-        return;
+bool checkprime(int n, int a = 2) {
+    if (n == 0 || n == 1 || n == a) {
+    return true; 
     }
     
+    else if (n % a == 0){
+    return false; 
+    } 
     
+    a++;
+    return checkprime(n, a+1);
+}
+int main() {
+    int num;
+    
+    cout << "This Program Check Prime number " << endl;   
+    cout << "--------------------------------" << endl;
+    cout << "Enter number to check prime number: ";
+    cin >> num;
+    if (checkprime (num)){
+        cout << num << " is Prime" << endl;
+    }
+    else {
+        cout << num << " not Prime " << endl;
+    }
 }
